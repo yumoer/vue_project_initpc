f603b01679155e860496de20a8a5fd167540abe5
myumo <794267701@qq.com>
2021-12-24 16:26:59 +0800

    优化页面

--- src/utils/request.js
+++ src/utils/request.js
@@ -109,7 +109,6 @@ service.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencod
 
 // 请求拦截器
 service.interceptors.request.use(
-
   config => {
     // 登录流程控制中，根据本地是否存在token判断用户的登录情况
     // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token
